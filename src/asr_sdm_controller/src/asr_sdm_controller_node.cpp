c4f57e20cf59cedd379dcdd12b0853cd90cb9bcb
Xiaoyu0601-Wang <micheal.wang@hotmail.com>
2025-04-19 12:57:11 +0000

    chore: refactor msgs and functions names

--- src/asr_sdm_controller/src/asr_sdm_controller_node.cpp
+++ src/asr_sdm_controller/src/asr_sdm_controller_node.cpp
@@ -23,15 +23,22 @@ public:
   AsrSdmControllerNode() : Node("asr_sdm_controller"), count_(0)
   {
     RCLCPP_INFO(this->get_logger(), "Start");
-    pub_ros_info_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_hardware_ =
-      this->create_wall_timer(1000ms, std::bind(&AsrSdmControllerNode::timer_callback, this));
+    pub_heartbeat_ =
+      this->create_publisher<std_msgs::msg::String>("~/output/controller/heartbeat", 1);
+    pub_robot_cmd_ =
+      this->create_publisher<asr_sdm_control_msgs::msg::RobotCmd>("~/output/robot_cmd", 1);
+    timer_heartbeat_ =
+      this->create_wall_timer(1500ms, std::bind(&AsrSdmControllerNode::timer_heartbeat, this));
+    timer_robot_control_ =
+      this->create_wall_timer(100ms, std::bind(&AsrSdmControllerNode::timer_controller, this));
   }
 
 private:
-  void timer_callback()
+  void timer_heartbeat() { RCLCPP_INFO(this->get_logger(), "Control Node Heartbeat"); }
+
+  void timer_controller()
   {
-    RCLCPP_INFO(this->get_logger(), "Publishing");
+    RCLCPP_INFO(this->get_logger(), "Control Node Publishing");
     //      auto message = std_msgs::msg::String();
     //      message.data = "Hello, world! " + std::to_string(count_++);
     //      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
@@ -40,9 +47,10 @@ private:
 
   size_t count_;
 
-  rclcpp::TimerBase::SharedPtr timer_hardware_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_ros_info_;
-  rclcpp::Publisher<asr_sdm_hardware_msgs::msg::CanFrame>::SharedPtr pub_can_interface_;
+  rclcpp::TimerBase::SharedPtr timer_heartbeat_;
+  rclcpp::TimerBase::SharedPtr timer_robot_control_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_heartbeat_;
+  rclcpp::Publisher<asr_sdm_control_msgs::msg::RobotCmd>::SharedPtr pub_robot_cmd_;
 };
 
 int main(int argc, char * argv[])
--- src/asr_sdm_hardware/src/asr_sdm_hardware_node.cpp
+++ src/asr_sdm_hardware/src/asr_sdm_hardware_node.cpp
@@ -14,6 +14,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include "asr_sdm_control_msgs/msg/robot_cmd.hpp"
 #include "asr_sdm_hardware_msgs/msg/can_frame.hpp"
 #include <std_msgs/msg/string.hpp>
 
@@ -38,48 +39,37 @@ public:
 
     uart_can_ = std::make_unique<amp::UART_CAN>(uart_port, uart_baudrate);
 
-    pub_ros_info_ = this->create_publisher<std_msgs::msg::String>("~/output/topic", 10);
-    sub_interface_can_ = this->create_subscription<asr_sdm_hardware_msgs::msg::CanFrame>(
-      "~/input/can_frame", rclcpp::SensorDataQoS{}.keep_last(1),
-      std::bind(&AsrSdmHardwareNode::canFrameCallback, this, std::placeholders::_1));
+    pub_heartbeat_ =
+      this->create_publisher<std_msgs::msg::String>("~/output/hardware/heartbeat", 1);
+    sub_robot_cmd_ = this->create_subscription<asr_sdm_control_msgs::msg::RobotCmd>(
+      "~/input/robot_cmd", rclcpp::SensorDataQoS{}.keep_last(1),
+      std::bind(&AsrSdmHardwareNode::hardware_control, this, std::placeholders::_1));
 
-    timer_hardware_ =
-      this->create_wall_timer(1000ms, std::bind(&AsrSdmHardwareNode::timerPublishCallback, this));
+    timer_heartbeat_ =
+      this->create_wall_timer(1000ms, std::bind(&AsrSdmHardwareNode::timer_heartbeat, this));
     timer_imu_ =
-      this->create_wall_timer(500ms, std::bind(&AsrSdmHardwareNode::timerHardwareCallback, this));
+      this->create_wall_timer(500ms, std::bind(&AsrSdmHardwareNode::timer_hardware_ctrl, this));
   }
 
 private:
-  void canFrameCallback(const asr_sdm_hardware_msgs::msg::CanFrame::SharedPtr msg)
+  void hardware_control(const asr_sdm_control_msgs::msg::RobotCmd::SharedPtr msg)
   {
-    msg_can_frame_ = *msg;
-    uart_can_->sendMsg(msg->id, msg->rtr, msg->ext, msg->dlc, msg->data.data());
-    RCLCPP_INFO(
-      this->get_logger(), "Received CAN frame: id=%d, data=%d", msg_can_frame_.id,
-      msg_can_frame_.data[0]);
+    msg_robot_cmd_ = *msg;
   }
 
-  void timerPublishCallback()
-  {
-    // RCLCPP_INFO("Publishing");
-    RCLCPP_INFO(this->get_logger(), "Publishing");
-    //      auto message = std_msgs::msg::String();
-    //      message.data = "Hello, world! " + std::to_string(count_++);
-    //      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    //      publisher_->publish(message);
-  }
+  void timer_heartbeat() { RCLCPP_INFO(this->get_logger(), "Hardware Node Heartbeat"); }
 
-  void timerHardwareCallback() {}
+  void timer_hardware_ctrl() {}
 
   size_t count_;
 
-  rclcpp::TimerBase::SharedPtr timer_hardware_;
+  rclcpp::TimerBase::SharedPtr timer_heartbeat_;
   rclcpp::TimerBase::SharedPtr timer_imu_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_ros_info_;
-  rclcpp::Subscription<asr_sdm_hardware_msgs::msg::CanFrame>::SharedPtr sub_interface_can_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_heartbeat_;
+  rclcpp::Subscription<asr_sdm_control_msgs::msg::RobotCmd>::SharedPtr sub_robot_cmd_;
 
   amp::UART_CAN::Ptr uart_can_;
-  asr_sdm_hardware_msgs::msg::CanFrame msg_can_frame_;
+  asr_sdm_control_msgs::msg::RobotCmd msg_robot_cmd_;
 };
 
 int main(int argc, char * argv[])
